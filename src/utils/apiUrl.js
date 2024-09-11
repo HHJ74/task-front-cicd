5e19ac4571ed56bfca27a8a26ed2060d9ccd2026
HHJ74 <gpgus0704@gmail.com>
2024-09-11 10:17:40 +0900

    first commit

---
+++ src/utils/apiUrl.js
@@ -0,0 +1,15 @@
+const GET_TASKS_API_URL = 'https://todobackup.aiccdabipyeung.com/get_tasks';
+const POST_TASKS_API_URL = 'https://todobackup.aiccdabipyeung.com/post_task';
+const DELETE_TASKS_API_URL =
+  'https://todobackup.aiccdabipyeung.com/delete_task';
+const UPDATE_COMPLETED_TASKS_API_URL =
+  'https://todobackup.aiccdabipyeung.com/update_completed_task';
+const UPDATE_TASK_API_URL = 'https://todobackup.aiccdabipyeung.com/update_task';
+
+export {
+  GET_TASKS_API_URL,
+  POST_TASKS_API_URL,
+  DELETE_TASKS_API_URL,
+  UPDATE_COMPLETED_TASKS_API_URL,
+  UPDATE_TASK_API_URL,
+};
